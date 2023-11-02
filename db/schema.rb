0499dcee756ca05bcec16201162c2c6f64925322
Grace Zhang <gracezhang@umail.ucsb.edu>
2023-11-02 10:30:41 -0700

    Created posts linked to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_163654) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_02_172611) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
