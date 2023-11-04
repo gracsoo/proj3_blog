b12924e75c32fad33ec4af0001fbc3a9d720d806
Grace Zhang <gracezhang@umail.ucsb.edu>
2023-11-04 12:58:45 -0700

    Linked posts to users and enabled users to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,19 +20,34 @@ class PostsController < ApplicationController
   end
 
   # POST /posts or /posts.json
-  def create
-    @post = Post.new(post_params)
+#   def create
+#     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+#     respond_to do |format|
+#       if @post.save
+#         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+#         format.json { render :show, status: :created, location: @post }
+#       else
+#         format.html { render :new, status: :unprocessable_entity }
+#         format.json { render json: @post.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+  def create
+    # debugger
+    @user = User.find_by_id(params[:user_id])
+    @post = @user.posts.new(content: params[:content], user_id: params[:user_id])
+
+
+    if !@post.save
+        flash[:notice] = @post.errors.full_messages.to_sentence
     end
+
+    redirect_to user_path(params[:user_id])
   end
+
+
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
@@ -65,6 +80,7 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:content, :user_id)
+      #params.require(:post).permit(:content, :user_id)
+      params.require(:post).permit(:content).merge(user_id: params[:user_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-  resources :users
+  resources :users do
+    resources :posts, only: [:create]
+  end
+  # resources :users
+
   resources :posts
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
